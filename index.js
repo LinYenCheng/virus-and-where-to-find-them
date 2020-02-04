a5735edfc6dc43f8b6303b6ad5354a3eaaf9dabc
yencheng <yencheng@mitake.com.tw>
2020-02-04 10:36:34 +0800

    feat: 新增折線圖表

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ import srcVirus from "./virus.png";
 const map = L.map("map").setView([23.5, 120.644], 5);
 
 const tiles = L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    attribution:
-      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }
 ).addTo(map);
 
@@ -57,12 +55,12 @@ axios
         minOpacity: 0.5
       }).addTo(map);
 
-      map.on("zoomend", function() {
-        const zoomlevel = map.getZoom();
-        if (zoomlevel < 5) {
+      map.on("zoomend", function () {
+        const zoomLevel = map.getZoom();
+        if (zoomLevel < 5) {
           map.removeLayer(cities);
         }
-        if (zoomlevel >= 5) {
+        if (zoomLevel >= 5) {
           if (map.hasLayer(cities)) {
             console.log("layer already added");
           } else {
@@ -72,3 +70,37 @@ axios
       });
     })
   );
+
+
+axios.get('https://cors-anywhere.herokuapp.com/https://infogram.com/api/live/flex/01a8508e-9cc9-4b64-994b-cb4fdc2ee6f4/831f2b9f-88fd-4cf1-a323-17756db0c7a1').then(
+  (resChart) => {
+    const dates = [];
+    const chinaPatientCounts = [];
+    const otherPatientCounts = [];
+    resChart.data.data[0].filter(elm => elm[0] !== '日期').forEach(elm => {
+      dates.push(elm[0]);
+      chinaPatientCounts.push(elm[1]);
+      otherPatientCounts.push(elm[2]);
+    });
+    const chart = c3.generate({
+      bindto: '#chart--line',
+      data: {
+        x: 'date',
+        xFormat: '%m/%d',
+        columns: [
+          ['date', ...dates],
+          ['中國病例', ...chinaPatientCounts],
+          ['其他病例', ...otherPatientCounts]
+        ]
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%m/%d'
+          }
+        }
+      }
+    });
+  }
+)
