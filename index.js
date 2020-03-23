0142660320f02c25cd0577f52adb8302c234299f
LinYenCheng <breaking0215@hotmail.com>
2020-03-24 07:47:41 +0800

    feat: add loading spinner and refactor

--- index.js
+++ index.js
@@ -22,219 +22,210 @@ const virusIcon = L.icon({
 let addressPoints = [];
 let cityMarkers = [];
 
-axios
-  .get(
-    "https://cors-anywhere.herokuapp.com/https://api.coronatracker.com/v2/stats/diff/global"
-  )
-  .then(resChart => {
-    const dates = [];
-    const diffConfirmCounts = [];
-    const confirmPatientCounts = [];
-    const deathCounts = [];
-    const recoverCounts = [];
+function generateChart(resChart) {
+  const dates = [];
+  const diffConfirmCounts = [];
+  const confirmPatientCounts = [];
+  const deathCounts = [];
+  const recoverCounts = [];
 
-    resChart.data.forEach(elm => {
-      dates.push(elm.ytd.toString().substring(0, 10));
-      diffConfirmCounts.push(elm.diffConfirmed);
-      confirmPatientCounts.push(elm.todayConfirmed);
-      deathCounts.push(elm.todayDeath);
-      recoverCounts.push(elm.todayRecover);
-    });
-    const chart = c3.generate({
-      bindto: "#chart--line",
-      data: {
-        x: "date",
-        xFormat: "%Y-%m-%d",
-        columns: [
-          ["date", ...dates],
-          ["增加數量", ...diffConfirmCounts],
-          ["全球確診病例", ...confirmPatientCounts]
-        ],
-        axes: {
-          全球確診病例: "y",
-          增加數量: "y2"
+  resChart.data.forEach(elm => {
+    dates.push(elm.ytd.toString().substring(0, 10));
+    diffConfirmCounts.push(elm.diffConfirmed);
+    confirmPatientCounts.push(elm.todayConfirmed);
+    deathCounts.push(elm.todayDeath);
+    recoverCounts.push(elm.todayRecover);
+  });
+  const chart = c3.generate({
+    bindto: "#chart--line",
+    data: {
+      x: "date",
+      xFormat: "%Y-%m-%d",
+      columns: [
+        ["date", ...dates],
+        ["增加數量", ...diffConfirmCounts],
+        ["全球確診病例", ...confirmPatientCounts]
+      ],
+      axes: {
+        全球確診病例: "y",
+        增加數量: "y2"
+      }
+    },
+    axis: {
+      x: {
+        type: "timeseries",
+        tick: {
+          format: "%m-%d"
         }
       },
-      axis: {
-        x: {
-          type: "timeseries",
-          tick: {
-            format: "%m-%d"
-          }
-        },
-        y2: {
-          show: true
-        }
+      y2: {
+        show: true
       }
-    });
-
-    const chartBar = c3.generate({
-      bindto: "#chart--bar",
-      data: {
-        x: "date",
-        xFormat: "%Y-%m-%d",
-        columns: [
-          ["date", ...dates],
-          ["死亡數量", ...deathCounts],
-          ["恢復數量", ...recoverCounts]
-        ],
-        axes: {
-          死亡數量: "y",
-          恢復數量: "y2"
-        }
-      },
-      axis: {
-        x: {
-          type: "timeseries",
-          tick: {
-            format: "%m-%d"
-          }
-        },
-        y2: {
-          show: true
-        }
-      }
-    });
-    const { todayConfirmed, todayDeath, todayRecover } = resChart.data[0];
-    var chartDounut = c3.generate({
-      bindto: "#chart--dounut",
-      data: {
-        columns: [
-          ["治療中", todayConfirmed - todayDeath - todayRecover],
-          ["恢復", todayRecover],
-          ["死亡", todayDeath]
-        ],
-        type: "donut"
-      },
-      donut: {
-        title: "武漢肺炎",
-        label: {
-          format: function(value, ratio, id) {
-            return value;
-          }
-        }
-      }
-    });
+    }
   });
 
-$.get(
-  "https://cors-anywhere.herokuapp.com/https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.csv",
-  function(data) {
-    var data = JSON.parse(csvJSON(data));
-    $(data).each(function(k, v) {
-      const nowIndex = locations.findIndex(elm => elm.location === v["縣市"]);
-      if (locations[nowIndex]) {
-        if (locations[nowIndex].count) {
-          locations[nowIndex].count += 1;
-        } else {
-          locations[nowIndex].count = 1;
+  const chartBar = c3.generate({
+    bindto: "#chart--bar",
+    data: {
+      x: "date",
+      xFormat: "%Y-%m-%d",
+      columns: [
+        ["date", ...dates],
+        ["死亡數量", ...deathCounts],
+        ["恢復數量", ...recoverCounts]
+      ],
+      axes: {
+        死亡數量: "y",
+        恢復數量: "y2"
+      }
+    },
+    axis: {
+      x: {
+        type: "timeseries",
+        tick: {
+          format: "%m-%d"
+        }
+      },
+      y2: {
+        show: true
+      }
+    }
+  });
+  const { todayConfirmed, todayDeath, todayRecover } = resChart.data[0];
+  var chartDounut = c3.generate({
+    bindto: "#chart--dounut",
+    data: {
+      columns: [
+        ["治療中", todayConfirmed - todayDeath - todayRecover],
+        ["恢復", todayRecover],
+        ["死亡", todayDeath]
+      ],
+      type: "donut"
+    },
+    donut: {
+      title: "武漢肺炎",
+      label: {
+        format: function(value, ratio, id) {
+          return value;
         }
       }
-    });
-    axios
-      .all([
-        axios.get(
-          "https://cors-anywhere.herokuapp.com/https://api.coronatracker.com/analytics/country"
-        ),
-        axios.get(
-          "https://cors-anywhere.herokuapp.com/https://api.coronatracker.com/v2/analytics/area?limit=100"
+    }
+  });
+}
+
+axios
+  .all([
+    axios.get(
+      "https://cors-anywhere.herokuapp.com/https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.csv"
+    ),
+    axios.get(
+      "https://cors-anywhere.herokuapp.com/https://api.coronatracker.com/analytics/country"
+    ),
+    axios.get(
+      "https://cors-anywhere.herokuapp.com/https://api.coronatracker.com/v2/analytics/area?limit=100"
+    ),
+    axios.get(
+      "https://cors-anywhere.herokuapp.com/https://api.coronatracker.com/v2/stats/diff/global"
+    )
+  ])
+  .then(
+    axios.spread((resTaiwan, resCountry, resChina, resChart) => {
+      $(".loading__overlay").css("zIndex", -1);
+      $(".loading__content").css("zIndex", -1);
+      var data = JSON.parse(csvJSON(resTaiwan.data));
+      $(data).each(function(k, v) {
+        const nowIndex = locations.findIndex(elm => elm.location === v["縣市"]);
+        if (locations[nowIndex]) {
+          if (locations[nowIndex].count) {
+            locations[nowIndex].count += 1;
+          } else {
+            locations[nowIndex].count = 1;
+          }
+        }
+      });
+      resCountry.data
+        .filter(
+          elm =>
+            elm.country !== "China" &&
+            elm.country !== "Hong Kong" &&
+            elm.country !== "Taiwan*" &&
+            elm.country !== "N/A"
         )
-      ])
-      .then(
-        axios.spread((resCountry, resChina) => {
-          resCountry.data
-            .filter(
-              elm =>
-                elm.country !== "China" &&
-                elm.country !== "Hong Kong" &&
-                elm.country !== "Taiwan*" &&
-                elm.country !== "N/A"
-            )
+        .map(elm => [
+          elm.country,
+          elm.total_confirmed,
+          "確診",
+          `${elm.lat} ${elm.lng}`,
+          elm.country
+        ])
+        .concat(
+          resChina.data
+            .filter(elm => elm.state !== "N/A")
             .map(elm => [
-              elm.country,
+              elm.state,
               elm.total_confirmed,
               "確診",
               `${elm.lat} ${elm.lng}`,
-              elm.country
+              elm.state
             ])
-            .concat(
-              resChina.data
-                .filter(elm => elm.state !== "N/A")
-                .map(elm => [
-                  elm.state,
-                  elm.total_confirmed,
-                  "確診",
-                  `${elm.lat} ${elm.lng}`,
-                  elm.state
-                ])
-            )
-            .concat(
-              locations
-                .filter(elm => elm.count)
-                .map(elm => [
-                  elm.location,
-                  elm.count,
-                  "確診",
-                  `${elm.lat} ${elm.lng}`,
-                  elm.location
-                ])
-            )
-            .forEach(elm => {
-              let nowCount = parseInt(elm[1]);
-              const tempMarker = L.marker(elm[3].split(" "), {
-                icon: virusIcon
-              }).bindPopup(`${elm[4]}確診：${elm[1]}`);
-              cityMarkers.push(tempMarker);
-              while (nowCount--) {
-                const arrayLatLng = elm[3].split(" ");
-                if (nowCount > 1000) {
-                  addressPoints.push(
-                    getRandomAround(arrayLatLng, nowCount * 3)
-                  );
-                } else if (nowCount > 2000) {
-                  addressPoints.push(
-                    getRandomAround(arrayLatLng, nowCount * 1.5)
-                  );
-                } else if (nowCount > 3000) {
-                  addressPoints.push(
-                    getRandomAround(arrayLatLng, nowCount * 1.2)
-                  );
-                } else if (nowCount > 4000) {
-                  addressPoints.push(
-                    getRandomAround(arrayLatLng, nowCount * 1.1)
-                  );
-                } else {
-                  addressPoints.push(
-                    getRandomAround(arrayLatLng, nowCount * 100)
-                  );
-                }
-              }
-            });
-
-          const cities = L.layerGroup(cityMarkers).addTo(map);
-          const heat = L.heatLayer(addressPoints, {
-            radius: 25,
-            blur: 20,
-            minOpacity: 0.5
-          }).addTo(map);
-
-          map.on("zoomend", function() {
-            const zoomLevel = map.getZoom();
-            if (zoomLevel < 5) {
-              map.removeLayer(cities);
+        )
+        .concat(
+          locations
+            .filter(elm => elm.count)
+            .map(elm => [
+              elm.location,
+              elm.count,
+              "確診",
+              `${elm.lat} ${elm.lng}`,
+              elm.location
+            ])
+        )
+        .forEach(elm => {
+          let nowCount = parseInt(elm[1]);
+          const tempMarker = L.marker(elm[3].split(" "), {
+            icon: virusIcon
+          }).bindPopup(`${elm[4]}確診：${elm[1]}`);
+          cityMarkers.push(tempMarker);
+          while (nowCount--) {
+            const arrayLatLng = elm[3].split(" ");
+            if (nowCount > 1000) {
+              addressPoints.push(getRandomAround(arrayLatLng, nowCount * 3));
+            } else if (nowCount > 2000) {
+              addressPoints.push(getRandomAround(arrayLatLng, nowCount * 1.5));
+            } else if (nowCount > 3000) {
+              addressPoints.push(getRandomAround(arrayLatLng, nowCount * 1.2));
+            } else if (nowCount > 4000) {
+              addressPoints.push(getRandomAround(arrayLatLng, nowCount * 1.1));
+            } else {
+              addressPoints.push(getRandomAround(arrayLatLng, nowCount * 100));
             }
-            if (zoomLevel >= 5) {
-              if (map.hasLayer(cities)) {
-                console.log("layer already added");
-              } else {
-                map.addLayer(cities);
-              }
-            }
-          });
-        })
-      );
-  }
-);
+          }
+        });
+
+      generateChart(resChart);
+
+      const cities = L.layerGroup(cityMarkers).addTo(map);
+      const heat = L.heatLayer(addressPoints, {
+        radius: 25,
+        blur: 20,
+        minOpacity: 0.5
+      }).addTo(map);
+
+      map.on("zoomend", function() {
+        const zoomLevel = map.getZoom();
+        if (zoomLevel < 5) {
+          map.removeLayer(cities);
+        }
+        if (zoomLevel >= 5) {
+          if (map.hasLayer(cities)) {
+            console.log("layer already added");
+          } else {
+            map.addLayer(cities);
+          }
+        }
+      });
+    })
+  );
 
 $("#btn-open").click(function() {
   $("#modal").css("opacity", 1);
