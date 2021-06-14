e7e90f2a9c5488540c41515a0d2ad4d247c9ed9e
LinYenCheng <breaking0215@hotmail.com>
2021-06-14 09:22:38 +0800

    style: 樣式

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,7 @@
 <script>
-  import Loading from './Loading.svelte';
-  import Map from './Map.svelte';
-  import Select from './Select.svelte';
+  import Loading from "./Loading.svelte";
+  import Map from "./Map.svelte";
+  import Select from "./Select.svelte";
 
   export let finalCountries = [];
 </script>
@@ -13,6 +13,13 @@
   <button id="btn-close">關閉圖表</button>
   <div id="chart--line" />
   <div id="chart--bar" />
+  <p>
+    <span>資料來源:</span>
+    <a href="https://github.com/CSSEGISandData/COVID-19" target="_blank"
+      >JHU CSSE COVID-19 Data</a
+    >
+  </p>
+  <p />
 </div>
 <div id="chart--dounut" />
 <Loading />
