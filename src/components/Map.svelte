84a45cb19ba15b468cb4a3e827406d4036eebdcc
LinYenCheng <breaking0215@hotmail.com>
2026-03-16 21:25:20 +0800

    style: change map style

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -31,7 +31,7 @@
     const map = L.map("map").setView([23.5, 120.8], 8);
     window.map = map;
     const tiles = L.tileLayer(
-      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
       {
         attribution:
           '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
